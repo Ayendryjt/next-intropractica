2cf37b955dbb693c75e12baa2452a73763ada158
Ayendryjt <127172475+Ayendryjt@users.noreply.github.com>
2023-10-29 17:48:14 -0400

    Style: Delete span and change width image
    Style FooterLink.tsx

--- src/app/components/atoms/FooterLink.tsx
+++ src/app/components/atoms/FooterLink.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 export const FooterLink = (props: { linkText: string }) => {
   return (
-    <>
-      <span>&bull;</span>
+    <>     
       <a
-        className="px-1 border-dashed border-b-2 border-b-yellow-js hover:border-black"
+        className="px-1 border-dashed  border-b-yellow-js hover:border-black"
         href="https://github.com/Ayendryjt/next-intropractica.git"
         rel="nofollow noreferrer"
         target="_blank"
@@ -13,7 +12,7 @@ export const FooterLink = (props: { linkText: string }) => {
         {props.linkText}
         <picture className="flex flex-center rounded-full">
             <img
-              width="40"
+              width="88"
               height="28"
               loading="lazy"
               src="https://i.imgur.com/vUiouM8.png"
